283993923977839c0cf645f39bbc95fa7c16abd5
Mari <marigalicer@gmail.com>
2019-03-19 17:04:03 -0400

    Add pickle for persisting pics dictionary between sessions to avoid 5 minute file loading on server startup.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import csv
+import pickle
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from img_to_vec import Img2Vec
@@ -18,21 +20,49 @@ input_path = './uploads/cats'
 img2vec = Img2Vec()
 pics = {}
 
-@app.before_first_request
+def create_pickle():
+    with open('vectors.pkl', 'wb') as output:
+        for file in os.listdir(input_path):
+            filename = os.fsdecode(file)
+            img = Image.open(os.path.join(input_path, filename))
+            vec = img2vec.get_vec(img)
+
+
+def create_csv():
+    with open('vectors.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',')
+        for file in os.listdir(input_path):
+            filename = os.fsdecode(file)
+            img = Image.open(os.path.join(input_path, filename))
+            vec = img2vec.get_vec(img)
+            print(vec)
+            filewriter.writerow([filename, vec])
+
 def create_file_dict():
-    for file in os.listdir(input_path):
-        filename = os.fsdecode(file)
-        img = Image.open(os.path.join(input_path, filename))
-        vec = img2vec.get_vec(img)
-        pics[filename] = vec
+    with open('vectors.pkl', 'wb') as output:
+        i = 0;
+        for file in os.listdir(input_path):
+            print(i)
+            filename = os.fsdecode(file)
+            img = Image.open(os.path.join(input_path, filename))
+            vec = img2vec.get_vec(img)
+            pics[filename] = vec
+        pickle.dump(pics, output, pickle.HIGHEST_PROTOCOL)
 
 def get_image_vector(filename):
     pil_image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     vec = img2vec.get_vec(pil_image)
     return vec
 
-def compare_image_vectors(vector1, vector2):
-    cosine_similarity(pics[pic_name].reshape((1, -1)), pics[key].reshape((1, -1)))[0][0]
+def find_similar_cat(user_image):
+    most_similar = 0
+    image_name = ""
+    for key in list(pics.keys()):
+        similarity = cosine_similarity(user_image.reshape((1, -1)), pics[key].reshape((1, -1)))[0][0]
+        if similarity > most_similar:
+            most_similar = similarity
+            image_name = key 
+    return image_name
 
 def allowed_file(fileName):
     return '.' in fileName and \
@@ -51,8 +81,9 @@ def classify():
         filename = secure_filename(image.filename)
         # Do the part where we pass the file to the model
         image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        get_image_vector(filename)
-    return "classifying image"
+        user_image = get_image_vector(filename)
+        return find_similar_cat(user_image)
 
 if __name__ == "__main__":
+    create_file_dict()
     app.run()
